81f421af2962b50bf0d5c0c415b244de9e29fd97
Daniel Gil <dgil@unedbarbastro.es>
2021-03-09 11:02:14 +0100

    init timeline refactor

--- dist/main.js
+++ dist/main.js
@@ -58458,11 +58458,12 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 
 
 jQuery( /*#__PURE__*/_asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-  var datasource;
+  var containerId, datasource;
   return regeneratorRuntime.wrap(function _callee$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
+          containerId = 'visualization';
           datasource = {
             "TimelineZoomable": false,
             "Name": "Timeline DEMO",
@@ -58541,9 +58542,9 @@ jQuery( /*#__PURE__*/_asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(fun
               "next": 11
             }]
           };
-          buildTimeline('visualization', datasource);
+          buildTimeline(containerId, datasource);
 
-        case 2:
+        case 3:
         case "end":
           return _context.stop();
       }
@@ -58567,55 +58568,11 @@ function buildTimeline(containerId, datasource) {
     start: datasource.TimelineStartDate,
     end: datasource.TimelineEndDate
   };
-  var timeline = new vis_timeline_standalone___WEBPACK_IMPORTED_MODULE_2__.Timeline(container, items, groups, options);
-  return timeline; // DOM element where the Timeline will be attached
-  //var container = document.getElementById('visualization');
-  // // Create a DataSet (allows two way data-binding)
-  // var items = new DataSet([
-  //     {id: 1, content: 'item 1', start: '2014-04-20'},
-  //     {id: 2, content: 'item 2', start: '2014-04-14'},
-  //     {id: 3, content: 'item 3', start: '2014-04-18'},
-  //     {id: 4, content: 'item 4', start: '2014-04-16', end: '2014-04-19'},
-  //     {id: 5, content: 'item 5', start: '2014-04-25'},
-  //     {id: 6, content: 'item 6', start: '2014-04-27', type: 'point'}
-  //     ]);
-  // // Configuration for the Timeline
-  // var options = {};
-  // // Create a Timeline
-  // var timeline = new Timeline(container, items, options);
-  // let now = new Date();
-  // let groups = new DataSet([
-  //     {id: 0, content: 'First', value: 1},
-  //     {id: 1, content: 'Third', value: 3},
-  //     {id: 2, content: 'Second', value: 2}
-  // ]);
-  // // create a dataset with items
-  // // note that months are zero-based in the JavaScript Date object, so month 3 is April
-  // let items = new DataSet([
-  //     {id: 0, group: 0, content: 'item 0', start: new Date(2014, 3, 17), end: new Date(2014, 3, 21)},
-  //     {id: 1, group: 0, content: 'item 1', start: new Date(2014, 3, 19), end: new Date(2014, 3, 20)},
-  //     {id: 2, group: 1, content: 'item 2', start: new Date(2014, 3, 16), end: new Date(2014, 3, 24)},
-  //     {id: 3, group: 1, content: 'item 3', start: new Date(2014, 3, 23), end: new Date(2014, 3, 24)},
-  //     {id: 4, group: 1, content: 'item 4', start: new Date(2014, 3, 22), end: new Date(2014, 3, 26)},
-  //     {id: 5, group: 2, content: 'item 5', start: new Date(2014, 3, 24), end: new Date(2014, 3, 27)}
-  // ]);
-  // // Configuration for the Timeline
-  // let options = {
-  //     orientation: {
-  //         axis: 'top'
-  //     },
-  //     groupOrder: 'id',
-  //     locale: 'es',
-  //     start: now.setDate(now.getDate() - 1),
-  //     end: now.setDate(now.getDate() + 8)
-  // };
-  // // Create a Timeline
-  // let timeline = new Timeline(container);
-  // timeline.setGroups(groups);
-  // timeline.setItems(items);
-  // timeline.setOptions(options);
-} //export default buildTimeline
-//module.exports = buildTimeline;
+  var timeline = new vis_timeline_standalone___WEBPACK_IMPORTED_MODULE_2__.Timeline(container);
+  timeline.setGroups(groups);
+  timeline.setItems(items);
+  timeline.setOptions(options);
+}
 })();
 
 /******/ })()
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Timeline } from "vis-timeline/standalone/";
 import { DataSet } from "vis-data/standalone";
 
 jQuery(async function () {
+    let containerId = 'visualization';
     let datasource = {
         "TimelineZoomable": false,
         "Name": "Timeline DEMO",
@@ -91,12 +92,11 @@ jQuery(async function () {
         ]
     };
 
-    buildTimeline('visualization', datasource);
+    buildTimeline(containerId, datasource);
 });
 
 function buildTimeline(containerId, datasource) {
     let container = document.getElementById(containerId);
-
     let items = new DataSet(datasource.Items);
     let groups = new DataSet(datasource.Groups);
 
@@ -113,65 +113,8 @@ function buildTimeline(containerId, datasource) {
         end: datasource.TimelineEndDate
     };
 
-    var timeline = new Timeline(container, items, groups, options);
-
-    return timeline;
-
-    // DOM element where the Timeline will be attached
-    //var container = document.getElementById('visualization');
-
-    // // Create a DataSet (allows two way data-binding)
-    // var items = new DataSet([
-    //     {id: 1, content: 'item 1', start: '2014-04-20'},
-    //     {id: 2, content: 'item 2', start: '2014-04-14'},
-    //     {id: 3, content: 'item 3', start: '2014-04-18'},
-    //     {id: 4, content: 'item 4', start: '2014-04-16', end: '2014-04-19'},
-    //     {id: 5, content: 'item 5', start: '2014-04-25'},
-    //     {id: 6, content: 'item 6', start: '2014-04-27', type: 'point'}
-    //     ]);
-
-    // // Configuration for the Timeline
-    // var options = {};
-
-    // // Create a Timeline
-    // var timeline = new Timeline(container, items, options);
-
-    // let now = new Date();
-    // let groups = new DataSet([
-    //     {id: 0, content: 'First', value: 1},
-    //     {id: 1, content: 'Third', value: 3},
-    //     {id: 2, content: 'Second', value: 2}
-    // ]);
-
-    // // create a dataset with items
-    // // note that months are zero-based in the JavaScript Date object, so month 3 is April
-    // let items = new DataSet([
-    //     {id: 0, group: 0, content: 'item 0', start: new Date(2014, 3, 17), end: new Date(2014, 3, 21)},
-    //     {id: 1, group: 0, content: 'item 1', start: new Date(2014, 3, 19), end: new Date(2014, 3, 20)},
-    //     {id: 2, group: 1, content: 'item 2', start: new Date(2014, 3, 16), end: new Date(2014, 3, 24)},
-    //     {id: 3, group: 1, content: 'item 3', start: new Date(2014, 3, 23), end: new Date(2014, 3, 24)},
-    //     {id: 4, group: 1, content: 'item 4', start: new Date(2014, 3, 22), end: new Date(2014, 3, 26)},
-    //     {id: 5, group: 2, content: 'item 5', start: new Date(2014, 3, 24), end: new Date(2014, 3, 27)}
-    // ]);
-
-    // // Configuration for the Timeline
-    // let options = {
-    //     orientation: {
-    //         axis: 'top'
-    //     },
-    //     groupOrder: 'id',
-    //     locale: 'es',
-    //     start: now.setDate(now.getDate() - 1),
-    //     end: now.setDate(now.getDate() + 8)
-    // };
-
-    // // Create a Timeline
-
-    // let timeline = new Timeline(container);
-    // timeline.setGroups(groups);
-    // timeline.setItems(items);
-    // timeline.setOptions(options);
+    let timeline = new Timeline(container);
+    timeline.setGroups(groups);
+    timeline.setItems(items);
+    timeline.setOptions(options);
 }
-
-//export default buildTimeline
-//module.exports = buildTimeline;
